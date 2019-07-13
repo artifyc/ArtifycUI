13471cd5b95c7c2bdf5dbce10cba6b1d9015f4b2
Nisha Bhuva <nishabhuva@Nishas-MacBook-Pro.local>
2019-07-13 12:40:11 -0400

    initial implementation of GridList

--- artifyc-ui/src/App.js
+++ artifyc-ui/src/App.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './style/App.css';
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import GridListTile from '@material-ui/core/GridListTile';
+import Example from './assets/example.jpg';
+import GridList from "@material-ui/core/GridList";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Artifyc!
-        </p>
+
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+        <GridListTile>
+          <img src={Example} alt="Example Pic"/>
+          <p> Hello </p>
+        </GridListTile>
+        <GridListTile>
+          <img src={Example} alt="Example Pic"/>
+          <p> Hello </p>
+        </GridListTile>
+        <GridListTile>
+          <img src={Example} alt="Example Pic"/>
+          <p> Hello </p>
+        </GridListTile>
+        </Grid>
+
+
       </header>
     </div>
   );

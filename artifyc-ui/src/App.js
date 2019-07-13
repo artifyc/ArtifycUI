1b10d7a36f9e3d23a935d61ea2d6f88e2d66ec3a
Nisha Bhuva <nishabhuva@Nishas-MacBook-Pro.local>
2019-07-13 12:04:55 -0400

    initialized and installed materialUI

---
+++ artifyc-ui/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './style/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello Artifyc!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

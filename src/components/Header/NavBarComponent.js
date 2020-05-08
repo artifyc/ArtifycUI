de449380e96a6801418c10f263b1891d3cd3fed6
Kyle Al-Shafei <kalshafei@gmail.com>
2020-05-08 19:57:20 -0400

    sexy shit yo, navbar cleaned up with new setup

--- src/components/Header/NavBarComponent.js
+++ src/components/Header/NavBarComponent.js
@@ -2,18 +2,11 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
 import logo from '../../assets/logo.jpg'
 import { Link } from "react-router-dom"
 import { Auth } from 'aws-amplify';
 import Button from '@material-ui/core/Button';
-
+import DropButton from './dropdownButton'
 
 const useStyles = makeStyles((theme) => ({
   grow: {
@@ -57,42 +50,18 @@ const useStyles = makeStyles((theme) => ({
 
 export default function PrimarySearchAppBar(props) {
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
 
-  const isMenuOpen = Boolean(anchorEl);
-
-  const handleProfileMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
- 
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-  };
-  const handleMenuCloseAndSignout = () => {
-    setAnchorEl(null);
-    Auth.signOut({ global: true })
-  };
-
-
-  const menuId = 'primary-search-account-menu';
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem component={Link} to="/Profile" onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem component={Link} to="/Dashboard" onClick={handleMenuClose}>Dashboard</MenuItem>
-      <MenuItem component={Link} to="/Settings" onClick={handleMenuClose}>Settings</MenuItem>
-      <MenuItem component={Link} to="/" onClick={handleMenuCloseAndSignout}>Sign Out</MenuItem>
-    </Menu>
-  );  
+  const descriptionArray = [
+    {
+        title: "Messages",
+    },
+    {
+        title: "Notifications",
+    },
+    {
+        title: "Account",
+    }
+]
 
   return (
     <div className={classes.grow}>
@@ -103,43 +72,20 @@ export default function PrimarySearchAppBar(props) {
           </Link>
           <div className={classes.grow} />
           { props.loggedIn &&
-
             <div className={classes.sectionDesktop}>
-              <IconButton aria-label="show 4 new mails" color="inherit">
-                <Badge badgeContent={0} color="secondary">
-                  <MailIcon />
-                </Badge>
-              </IconButton>
-              <IconButton aria-label="show 17 new notifications" color="inherit">
-                <Badge badgeContent={0} color="secondary">
-                  <NotificationsIcon />
-                </Badge>
-              </IconButton>
-              <IconButton
-                edge="end"
-                aria-label="account of current user"
-                aria-controls={menuId}
-                aria-haspopup="true"
-                onClick={handleProfileMenuOpen}
-                color="inherit" 
-              >
-                <AccountCircle />
-              </IconButton>
+              {descriptionArray.map(value => (
+                    <DropButton key={value.title} {...value}/>
+                ))}
             </div>
-
           }
-
           { !props.loggedIn &&
             <div>
               <Button className={classes.buttonBoxing} variant="contained" color="secondary" >Become a Creator</Button>
               <Button className={classes.buttonBoxing} variant="contained" color="primary" onClick={() => Auth.federatedSignIn()}>Sign In</Button>
-
             </div>
-
           }
         </Toolbar>
       </AppBar>
-      {renderMenu}
     </div>
   );
 }

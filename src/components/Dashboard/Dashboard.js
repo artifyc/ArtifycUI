276285b5d1bfb21999e919fad9be686a28893b14
Mingquan Ma <mingqma@umich.edu>
2020-05-30 16:33:39 -0400

    add makePutCall function in board, move setState to then() block

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,11 +4,21 @@ import Board from '@lourenci/react-kanban'
 
 const board = {
   "columns":
-[{"id":69,
-"title":"Backlog",
-"cards":[
-    {"id":1,"title":"Add card","description":"Ellery was here bitches"}]},
-    {"id":2,"title":"Doing","cards":[{"id":2,"title":"Drag-n-drop support","description":"AWS IS MY BITCH!!"}]}]}
+    [
+      {"id":69,
+        "title":"Initial Render",
+        "cards":[
+          {"id":1,"title":"This is the","description":"initial render"}
+        ]
+      },
+      {"id":2,
+        "title":"This should be",
+        "cards":[
+          {"id":2,"title":"updated when","description":"the user signs in"}
+        ]
+      }
+  ]
+}
 
 class Dashboard extends React.Component {
   constructor(props)  {
@@ -18,8 +28,23 @@ class Dashboard extends React.Component {
         board: null,
         checked: []
       };
-      console.log(this.props.currUser);
-    }
+    console.log(this.props.currUser);
+    this.makePutCall = this.makePutCall.bind(this);
+
+  }
+
+  makePutCall(board) {
+      fetch('https://8vmazpdvrb.execute-api.us-east-1.amazonaws.com/qa/boards', {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json',
+          'Authorization': this.props.currUser.signInUserSession.idToken.jwtToken
+        },
+        body: JSON.stringify(board)
+      })
+  }
+
+
 
   componentDidUpdate(prevProps){
     if (this.props.currUser !== prevProps.currUser) {
@@ -31,7 +56,7 @@ class Dashboard extends React.Component {
       console.log('sending thing');
       console.log(this.props.currUser);
       console.log(this.props.currUser.signInUserSession.idToken.jwtToken);
-
+      var fetchedBoard = {}
       fetch('https://8vmazpdvrb.execute-api.us-east-1.amazonaws.com/qa/boards', {
           method: 'GET',
           headers: {
@@ -40,28 +65,47 @@ class Dashboard extends React.Component {
           },
         })
           .then(res => res.json())
-          .then(res => console.log(res.columns.columns.board.columns))
-          //.then(res => this.props.board = res.columns.columns.board.columns)
+          .then(res => {
+            fetchedBoard = res.columns.columns;
+            return fetchedBoard;
+          })
+          .then(fetchedBoard => {    
+            if (fetchedBoard !== prevProps.board) {
+              this.setState({
+                currUser: this.props.currUser,
+                board: fetchedBoard,
+                checked: 1
+              });
+
+              console.log("New board:");
+              console.log(this.state.board);
+            }
+          })
     }
 
-    if (this.props.board !== prevProps.board) {
-        this.setState ({
-          currUser: this.props.currUser,
-          board: this.props.board,
-          checked: 1
-        });
-
-        console.log("New board:");
-        console.log(this.state.board);
-      }
-    
   }
 
     render() {
         return (
-            <div>
-                <Board initialBoard={board} />
-            </div>
+          <Board
+            allowRemoveLane
+            allowAddColumn
+            allowRenameColumn
+            allowRemoveCard
+            onLaneRemove={this.makePutCall}
+            onCardRemove={this.makePutCall}
+            onLaneRename={this.makePutCall}
+            onCardDragEnd={this.makePutCall}
+            onColumnDragEnd={this.makePutCall}
+            initialBoard={board}
+            allowAddCard={{ on: "top" }}
+            onNewCardConfirm={draftCard => ({
+              id: new Date().getTime(),
+              ...draftCard
+            })}
+            onCardNew={this.makePutCall}
+          />
+    
         )
     }
 }
